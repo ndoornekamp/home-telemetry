21261ef74a5ea30bce8864a1ba9c347c78ccd401
Nick Doornekamp <nddoornekamp@gmail.com>
2023-06-24 15:27:55 +0200

    Move code to home_telemetry

---
+++ home_telemetry/data_analysis.py
@@ -0,0 +1,52 @@
+import datetime
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+from database import get_measurements
+from models import MeasurementType, Source
+
+if __name__ == "__main__":
+    p1_power_measurements = get_measurements(measurement_type=MeasurementType.POWER, source=Source.HOMEWIZARD_P1)
+    solax_power_measurements = get_measurements(measurement_type=MeasurementType.POWER, source=Source.SOLAX)
+
+    timestamps_p1 = [measurement.timestamp for measurement in p1_power_measurements]
+    values_p1 = [measurement.value for measurement in p1_power_measurements]
+
+    values_p1_avg, timestamps_p1_avg, values_p1_undersampled, timestamps_p1_undersampled = [], [], [], []
+    avg_values = []
+    avg_timestamp = timestamps_p1[0]
+    avg_period = datetime.timedelta(minutes=1)
+
+    for measurement in tqdm(p1_power_measurements):
+        if measurement.timestamp < avg_timestamp + avg_period:
+            avg_values.append(measurement)
+        else:
+            values_p1_avg.append(
+                sum([measurement.value for measurement in avg_values])/len(avg_values)
+            )
+            values_p1_undersampled.append(measurement.value)
+            timestamps_p1_undersampled.append(measurement.timestamp)
+            timestamps_p1_avg.append(avg_timestamp)
+            avg_timestamp = measurement.timestamp
+            avg_values = [measurement]
+
+    timestamps_solax = [measurement.timestamp for measurement in solax_power_measurements]
+    values_solax = [measurement.value for measurement in solax_power_measurements]
+
+    # Create plot
+    fig, ax = plt.subplots()
+    ax.plot(timestamps_p1, values_p1)
+    ax.plot(timestamps_p1_avg, values_p1_avg)
+    ax.plot(timestamps_p1_undersampled, values_p1_undersampled)
+    # ax.plot(timestamps_solax, values_solax)
+
+    # Format plot
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Value')
+    ax.set_title('Time Series Plot')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+
+    # Display plot
+    plt.show()
