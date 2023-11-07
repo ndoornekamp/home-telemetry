0e37d1ff779f791250cd4201262abad7413d5d98
Nick Doornekamp <nddoornekamp@gmail.com>
2023-11-07 19:32:14 +0100

    Fix typo in heatpump plots

--- data_analysis/heatpump.py
+++ data_analysis/heatpump.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-from datetime import datetime
 from data_analysis.plotting import display_plot
 from home_telemetry.database import get_measurements
 from home_telemetry.models import MeasurementType, Source
@@ -8,37 +7,38 @@ from home_telemetry.models import MeasurementType, Source
 
 def heatpump_analysis():
     heatpump_flow_measurements = get_measurements(
-        measurement_type=MeasurementType.FLOW, source=Source.HEISHAMON, datetime_gte=datetime(2023, 9, 23)
+        measurement_type=MeasurementType.FLOW, source=Source.HEISHAMON
     )
     heatpump_main_inlet_measurements = get_measurements(
         measurement_type=MeasurementType.TEMPERATURE,
         source=Source.HEISHAMON,
-        datetime_gte=datetime(2023, 9, 23),
         description="Main inlet",
+    )
+    heatpump_main_outlet_measurements = get_measurements(
+        measurement_type=MeasurementType.TEMPERATURE,
+        source=Source.HEISHAMON,
+        description="Main outlet",
     )
     heatpump_main_target = get_measurements(
         measurement_type=MeasurementType.TEMPERATURE,
         source=Source.HEISHAMON,
-        datetime_gte=datetime(2023, 9, 23),
         description="Main target",
     )
     dhw_target = get_measurements(
         measurement_type=MeasurementType.TEMPERATURE,
         source=Source.HEISHAMON,
-        datetime_gte=datetime(2023, 9, 23),
         description="DHW target",
     )
     dhw_actual = get_measurements(
         measurement_type=MeasurementType.TEMPERATURE,
         source=Source.HEISHAMON,
-        datetime_gte=datetime(2023, 9, 23),
         description="DHW actual",
     )
 
     timestamps_heatpump = [measurement.timestamp for measurement in heatpump_flow_measurements]
     values_heatpump_flow = [measurement.value for measurement in heatpump_flow_measurements]
     values_heatpump_inlet = [measurement.value for measurement in heatpump_main_inlet_measurements]
-    values_heatpump_outlet = [measurement.value for measurement in heatpump_main_inlet_measurements]
+    values_heatpump_outlet = [measurement.value for measurement in heatpump_main_outlet_measurements]
     values_heatpump_target = [measurement.value for measurement in heatpump_main_target]
     values_dhw_target = [measurement.value for measurement in dhw_target]
     values_dhw_actual = [measurement.value for measurement in dhw_actual]
